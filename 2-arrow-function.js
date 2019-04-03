d520ecee16b7d3a81993efec12d19eb129438e77
Ahmed Elattar <ahmedelattar73@gmail.com>
2019-04-03 19:02:52 +0200

    Arrow function

---
+++ 2-arrow-function.js
@@ -0,0 +1,17 @@
+// Normal function
+var sayHello = function (arg) {
+    console.log(arg);
+};
+// Arrow function with param
+var arrowFuncWithParam = function (arg) {
+    console.log(arg);
+    console.log(arg);
+};
+// Single line
+var arrowFuncWithParamSingleLine = function (arg) { return console.log(arg); };
+// without params 
+var arrowWithoutParams = function () {
+    console.log("Hello");
+    console.log("Hello");
+};
+var arrowWithoutParamsSingle = function () { return console.log("Hello"); };
---
+++ 2-arrow-function.ts
@@ -0,0 +1,20 @@
+// Normal function
+let sayHello = function(arg) {
+    console.log(arg);
+}
+
+// Arrow function with param
+let arrowFuncWithParam = (arg) => {
+    console.log(arg);
+    console.log(arg);
+}
+// Single line
+let arrowFuncWithParamSingleLine = (arg) => console.log(arg);
+
+// without params 
+let arrowWithoutParams = () => {
+    console.log("Hello");
+    console.log("Hello");
+}
+
+let arrowWithoutParamsSingle = () => console.log("Hello");
